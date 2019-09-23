ac4d89e98ad3708c29799c371a4cd43d4169dd5b
mcv1993 <mcv1993@gmail.com>
2019-09-23 23:45:27 +0000

    gems installed: figaro, simple form, bootstrap. static page controller and view created

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require jquery_ujs
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require popper
+//= require bootstrap-sprockets
+//= require tether
 //= require_tree .

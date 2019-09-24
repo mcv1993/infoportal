4fbf198b57f569213471d9e43d26f6335ac895cb
mcv1993 <mcv1993@gmail.com>
2019-09-24 01:57:24 +0000

    added devise, user/admin models/controllers

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,8 @@
+class AdminsController < ApplicationController
+  #before_filter :ensure_god_mode
+
+  #def ensure_god_mode
+  #redirect_to admin_root_path unless current_admin.god_mode?
+  #end
+  
+end

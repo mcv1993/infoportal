b413ab0a667b28a71e6276761092a78d216d7719
mcv1993 <mcv1993@gmail.com>
2019-09-25 05:22:23 +0000

    added admin login form to index page

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,14 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+
+
+  def super_admin
+    #if Admin.super?
+
+  end
+
+
 end
